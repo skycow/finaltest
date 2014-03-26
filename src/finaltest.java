775f911b1408a9c7f9f01efc912c009572caca40
Skyler Cowley <skyler.cowley@gmail.com>
2014-03-26 11:05:54 -0600

    initial class commit

--- src/finaltest.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Skyler on 3/26/14.
- */
-public class finaltest {
-}
---
+++ src/finaltistclass.java
@@ -0,0 +1,25 @@
+public class finaltistclass {
+    //this is a class to create a search engine
+
+    //we need to parse files
+
+    //and we need to store the data
+
+    //we then need to search the data
+
+    //and we need to desplay the results
+
+    //and now it should work?
+
+    //what happens if i do this
+
+    //and now a new final comment
+
+    //
+
+    //comment
+   public static void main(String [] args){
+        System.out.println("hello world");
+        System.exit(0);
+    }
+}
